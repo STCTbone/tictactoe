f068e8957748e375179ef5fe117698aee3950ee2
Matt Rieger <matthew.rieger@gmail.com>
2013-04-24 00:47:39 -0500

    changed some specs to reflect that the AI now moves first

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -11,20 +11,20 @@ describe Game do
     context "it's the player's move" do
       it "marks the appropriate square with an X" do
         game.move(5, :player)
-        game.squares[5].should == "X"
+        game.squares[5].should == "O"
       end
     end
     context "it is the AI's move" do
       it "marks the appropriate square with an O" do
         game.move(5, :ai)
-        game.squares[5].should == "O"
+        game.squares[5].should == "X"
       end
     end
   end
   describe "game.ai_move" do
     it "return a blocking move for the AI to make" do
-      game.move(0, :ai)
-      game.move(1, :ai)
+      game.move(0, :player)
+      game.move(1, :player)
       game.ai_move.should == 2
     end
   end
