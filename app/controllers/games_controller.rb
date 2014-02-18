cf2eab641c2e711f0d609d269d40155741a79e0f
Matthew Rieger <matthew.rieger@gmail.com>
2014-02-17 21:58:09 -0600

    Refactor Game#Move, extracting some complexity

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,32 +1,39 @@
 class GamesController < ApplicationController
   def index
   end
+
   def create
     game = Game.new(squares: Array.new(9))
     game.move(4, :ai)
     game.save
     redirect_to game_path(game)
   end
+
   def show
-    @game = Game.find_by_id(params[:id])    
+    @game = Game.find_by_id(params[:id])
     @winner = GameKeeper::winner(@game)
     gon.winner = ""
 
   end
+
   def move
     @game = Game.find_by_id(params[:game])
-    @player_square = params[:square].to_i
-    @game.move(@player_square, :player)
-    @ai_square = GameKeeper.ai_move(@game)
-    @game.move(@ai_square, :ai) unless @game.squares.compact.count == 9
-    @winner = GameKeeper.winner(@game)# need to convert nil to an empty string because javascript recognizes null, not nil
+    @player_square, @ai_square = make_moves(@game, params[:square].to_i)
+    @winner = GameKeeper.winner(@game)
     gon.winner = @winner
 
-    respond_to do |format|
-    # # format.html redirect_to game_path(@game)
-    #  format.json { head :no_content }
-      format.js {}
-    end
-
+    respond_to :js
   end
+
+  private
+
+  def make_moves(game,player_square)
+    game.move(player_square, :player)
+    ai_square = GameKeeper.ai_move(@game)
+    game.move(ai_square, :ai) unless game.squares.compact.count == 9
+
+    return player_square, ai_square
+  end
+
+
 end
