2ff2863176f0a7a9cd80674598f6f56250dd5925
Matthew Rieger <matthew.rieger@gmail.com>
2014-02-17 21:40:40 -0600

    Removed old minmax code from game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,41 +13,4 @@ class Game < ActiveRecord::Base
     self.save
   end
 
-  #implementing the minimax algorithm
-  
- # def best_move
- #   return self.moves.max { |a, b| a.score <=> b.score  }
- # end
-
-  
-  def score
-    if self.winner == 'X'
-        return -1
-    elsif self.winner == 'O'
-    	return 1
-    elsif self.squares.compact.count == 9 #a draw
-    	return 0
-    else # the game isn't over yet
-    	lower_scores = self.moves.map { |move| move.score}
-    	self.player == 'X' ? lower_scores.min : lower_scores.max
-    end   
-  end
-
-def generate_moves
-    next_player = (self.player_turn == 'X' ? 'O' : 'X')
-    puts self.squares[0]
-    self.squares.each_with_index do |square, position|
-      puts square
-      unless square
-        next_squares = self.squares.dup
-        next_squares[position] = self.player_turn
-
-        next_game = Game.create(player_turn: next_player, squares: next_squares)
-        self.moves << next_game
-        next_game.generate_moves
-        self.save
-      end
-    end
-end
-
 end
